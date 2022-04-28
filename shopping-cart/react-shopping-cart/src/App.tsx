11f64bae6431f725d676320b9e88580238867b7e
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-04-28 14:24:40 -0400

    fetch data from fake store api

--- shopping-cart/react-shopping-cart/src/App.tsx
+++ shopping-cart/react-shopping-cart/src/App.tsx
@@ -11,7 +11,28 @@ import Badge from '@material-ui/core/Badge';
 //styles
 import { Wrapper } from './App.styles';
 
+//types
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  quantity: number;
+}
+
+//get products from fake store API
+const getProducts = async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+
+  if (isLoading) return <LinearProgress/>;
+  if (error) return <div>Something went wrong!</div>;
+
+
   return (
     <div className="App">
     </div>
